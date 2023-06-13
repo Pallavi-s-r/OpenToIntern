681d69edbe17aa88f9dfbf133b977a51a76ec3d0
Pallavi-s-r <pallavisingh.sgrl9@gmail.com>
2023-06-13 21:44:02 +0530

    backend

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -3,7 +3,13 @@ const InternModel = require('../models/internModel');
 
 const createCollege = async function(req,res){
  try{  
+    const data = req.body;
+    const {name, fullName, logoLink} = data;
 
+
+if(Object.keys(data).length===0){
+    return res.status(400).send({status:false,message:"Please enter valid College details"});
+}
     const createdData = await CollegeModel.create(data)
 
     return res.status(201).send({status:true,data:createdData});
@@ -15,30 +21,47 @@ const createCollege = async function(req,res){
 
 //--------------------------------------------------------------------------------------------------------------------------
 
-const getInternDetails = async(req,res)=>{
-    try{
-        const abbrevatedCollegeName = req.query.abbrevatedCollegeName;
+// const getCollegeDetails = async(req,res)=>{
+//     try{
+//         const abbrevatedCollegeName = req.query.abbrevatedCollegeName;
 
-        const college = await CollegeModel.findOne({name:abbrevatedCollegeName});
+//         const college = await CollegeModel.findOne({name:abbrevatedCollegeName});
 
-        if(!college){
-            return res.status(404).send({status:false,message:"no such college present"});
-        }
-        const interns = await InternModel.find({collegeId:college._id, isDeleted:false}).select({collegeId:0,isDeleted:0,__v:0});
+//         if(!college){
+//             return res.status(404).send({status:false,message:"no such college present"});
+//         }
+//         const interns = await InternModel.findOne({collegeId:college._id, isDeleted:false}).select({collegeId:0,isDeleted:0,__v:0});
         
-        if(!interns){
-            return res.status(400).send({status:false,message:"No applicants for internships"});
-        }
-        const responseData = {
-            name: college.name,
-            fullName: college.fullName,
-            logoLink: college.logoLink,
-            interns: interns
-          };
+//         if(!interns){
+//             return res.status(404).send({status:false,message:"No applicants for internships"});
+//         }
+//         const responseData = {
+//             name: college.name,
+//             fullName: college.fullName,
+//             logoLink: college.logoLink,
+//             interns: interns
+//           };
           
-        return res.status(200).send({status:true, data:responseData});
-    }catch(error){
-        return res.status(500).send({status:false,message:error.message});
-    }
+//         return res.status(200).send({status:true, data:responseData});
+//     }catch(error){
+//         return res.status(500).send({status:false,message:error.message});
+//     }
+// }
+
+const getDetails = async function(req,res){
+try{
+    const shrtName = req.query.shrtName;
+    const college = await CollegeModel.findOne({name:shrtName});
+    const interns = await InternModel.find({collegeId:college._id, isDeleted:false});
+    const responseData ={
+        name:college.name,
+        fullName:college.fullName,
+        logoLink:college.logoLink,
+        interns:interns
+    };
+   return res.send({status:true, message:responseData});
+}catch(error){
+    res.send({status:false, mesage:error.mesage});
 }
-module.exports = {createCollege,getInternDetails};
+}
+module.exports = {createCollege,getDetails};
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 const CollegeContrllr = require('../controller/collegeController');
 const InternControllr = require('../controller/internController');
 const Middleware = require('../middleware/middleware')
 
 router.post('/functionup/colleges', Middleware.collegeValidation, CollegeContrllr.createCollege);
 router.post('/functionup/interns', Middleware.InternValidations, InternControllr.createIntern);
-router.get('/functionup/collegeDetails', CollegeContrllr.getInternDetails);
+router.get('/functionup/collegeDetails', CollegeContrllr.getCollegeDetails);
 
 module.exports = router;
